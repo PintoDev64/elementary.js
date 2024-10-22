729fa55eb603ce162610596b542a8a417a39b6d6
PintoDev64 <pintogamer64@gmail.com>
2024-10-22 16:00:54 -0500

    Small Fix
    useLocation solo manejaba la ruta global (Browser)

--- src/library/hooks/useLocation.ts
+++ src/library/hooks/useLocation.ts
@@ -1,16 +1,21 @@
 import { useContext } from "react"
-import { BrowserRoutingContext } from "../context"
+import { BrowserRoutingContext, LocalRoutingContext } from "../context"
+
+interface locationParams {
+    local?: string
+}
 
 export default function useLocation() {
+    const GlobalContext = useContext(BrowserRoutingContext);
+    const LocalContext = useContext(LocalRoutingContext)
 
-    const Context = useContext(BrowserRoutingContext)
+    const location = ({ local }: locationParams) => {
+        if (GlobalContext === null || LocalContext === null) throw new RangeError("la funcion no se encuentra dentro del contexto del enrutador");
 
-    const location = () => {
-        if (Context === null) {
-            throw new RangeError("la funcion no se encuentra dentro del contexto del enrutador")
-        }
+        const ArrayIndex = LocalContext.state.findIndex(({ Name }) => Name === local);
 
-        return Context.Path
+        if (local === undefined) return GlobalContext.Path
+        else if (local?.length >= 1) return LocalContext.state[ArrayIndex].Path
     }
 
     return {
